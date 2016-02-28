874796ca93d3a5c29971a15fe5f0f0c32c9a8011
Pavlo15 <Pavlo.mysak1@gmail.com>
2016-02-28 13:39:51 +0200

    1-st Привязка до БД

---
+++ Kyrsu/Kyrsu/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;  
+
+namespace Kyrsu
+{
+    public partial class Form1 : Form
+    {
+        string conectionString;
+        public Form1()
+        {
+            InitializeComponent();
+            conectionString = @" Data Source = C:\ProjectGit\Kyrsu\BDKyrsu.db; Version = 3 "; 
+        }
+        SQLiteDataAdapter mda;
+        SQLiteConnection mcon;
+        DataTable dt = new DataTable();
+        int ii = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mcon = new SQLiteConnection(conectionString);
+            mda = new SQLiteDataAdapter(" select * from Test1", mcon);
+            mda.Fill(dt);
+        }
+}
